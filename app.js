f951803dcab01881c4008880a6b401ad54478bfe
Lucas <lucas.desa@outlook.com.br>
2023-01-27 14:18:55 -0300

    Instal nodemon

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+  res.send('<h1>Olá, mundo hahahha gay</h1>')
+})
+
+app.get('/worker', (req, res) => {
+  res.json({name:'Maria', salary: 3000, department: 'Marketing' })
+})
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado :)')
+})
